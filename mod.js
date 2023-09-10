17723f3a8c161d72aa345d414535d945a607c1a0
Tailsrus <109730620+Tailsrus@users.noreply.github.com>
2023-09-10 12:59:08 +0500

    Add files via upload

---
+++ mod.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "TUHC Experience 05492 & 05777", 
+  summary: "1",
+  author: "Tailsrus",
+  description: "1",
+  modVersion: 1.0,
+
+  edit: true,
+
+  trees: {
+    "./Assets/": "assets://"
+  },
+
+	edit(archive) {
+	} // it's just that the mod doesn't work if there isn't a single function.
+
+}
+      
+
+   
